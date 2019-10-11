5f6aeed8c04d52c0fed6b53b6442aa1be20c1e4b
Yayan Adipraja <yayanadipraja@gmail.com>
2019-10-11 13:51:31 +0700

    App_id changes

--- helper.py
+++ helper.py
@@ -1,7 +1,9 @@
+import os
 class Helper:
     def search_customer(participants):
+        app_id = os.getenv("QISMO_APP_ID")
         for p in participants:
-            if "customer_appu-bxo0bgp6agfyrsof@qismo.com" in p["email"]:
+            if f"customer_{app_id}@qismo.com" in p["email"]:
                 recipient_id = p["email"].split("_")[0]
                 return recipient_id
 
